9fe7dbb55175f45e0d4c95ff4c7af60956c4d438
DiegoHenrique0 <83367342+DiegoHenrique0@users.noreply.github.com>
2025-10-19 20:29:50 -0300

    landing pag

---
+++ script.js
@@ -0,0 +1,39 @@
+// responsividade da nav bar
+document.addEventListener('DOMContentLoaded', function() {
+    const toggler = document.querySelector('.navbar-toggler');
+    const navLinks = document.querySelector('.navbar-links');
+
+    toggler.addEventListener('click', function() {
+        navLinks.classList.toggle('active'); // Adiciona/remove a classe 'active' nos links
+        toggler.classList.toggle('active'); // Adiciona/remove a classe 'active' no toggler para a animação
+        const expanded = toggler.getAttribute('aria-expanded') === 'true' || false;
+        toggler.setAttribute('aria-expanded', !expanded); // Atualiza o atributo ARIA
+    });
+});
+
+//imagens automaticas
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = [
+    '/site/imagens/divisoria_2_1920.jpg',
+    '/site/imagens/divisoria_3_1920.jpg',
+
+       
+        // Adicione todos os caminhos das suas imagens
+    ];
+
+    let currentIndex = 0;
+    const imageElement = document.getElementById('background-image-slider');
+    const intervalTime = 4500; // Tempo em milissegundos (4 segundos para dar tempo de ler)
+
+    function changeImage() {
+        // Incrementa o índice e volta ao início se chegar ao final
+        currentIndex = (currentIndex + 1) % images.length;
+        
+        // Aplica a nova imagem
+        imageElement.src = images[currentIndex];
+    }
+
+    // Inicia a troca de imagens em um intervalo regular
+    setInterval(changeImage, intervalTime);
+});
